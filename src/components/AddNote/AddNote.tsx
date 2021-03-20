c0527e1836f5297906891b2722c34b995e174fa8
SDen4 <asw325@yandex.ru>
2021-03-20 19:20:27 +0500

    initial logic in AddNote

--- src/components/AddNote/AddNote.tsx
+++ src/components/AddNote/AddNote.tsx
@@ -1,20 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './AddNote.module.scss';
 
 const AddNote: React.FC = () => {
+  // Название заметки
+  const [noteTitle, setТoteTitle] = useState('');
+  // Новая задача
+  const [noteTask, setNoteTask] = useState('');
+  // Массив новых задач
+  const [tasks, setTasks] = useState([] as any);
+
+  // Получаю значение из инпута названия новой заметки
+  const noteTitleHandler = (event: any) => {
+    let value = event.target.value;
+    setТoteTitle(value);
+  };
+
+  // Получаю значение из инпута новой задачи
+  const noteTaskHandler = (event: any) => {
+    let value = event.target.value;
+    setNoteTask(value);
+    console.log(noteTask);
+  };
+
+  // Добавление новой задачи
+  const submitTaskHandler = (event: any) => {
+    event.preventDefault();
+    console.log(noteTask);
+
+    // Добавить новую задачу в массив задач
+    setTasks([...tasks, noteTask]);
+
+    // Очистить инпут после добавления
+    setNoteTask('');
+  };
+
+  // Submit всей формы добавления новой заметки и всех задач
+  const submitNoteHadler = (event: any) => {
+    event.preventDefault();
+
+    const newNote = {
+      noteTitle,
+      id: Date.now().toString(),
+      tasks,
+    };
+    console.log(newNote);
+  };
+
   return (
     <div className={styles.component}>
       <h2>Создание новой заметки</h2>
 
-      <form onSubmit={() => null} className={styles.form}>
+      <form onSubmit={submitNoteHadler} className={styles.form}>
         <div className={styles.row}>
-          <input type="text" placeholder="Введите название заметки" />
+          <input
+            type="text"
+            value={noteTitle}
+            onChange={noteTitleHandler}
+            placeholder="Введите название заметки"
+          />
         </div>
 
         <div className={styles.row}>
-          <input type="text" placeholder="Введите название задачи" />
-          <button type="button" className={styles.button}>
+          <input
+            type="text"
+            value={noteTask}
+            onChange={noteTaskHandler}
+            placeholder="Введите название задачи"
+          />
+          <button
+            type="button"
+            className={styles.button}
+            onClick={submitTaskHandler}
+          >
             Добавить задачу
           </button>
         </div>
